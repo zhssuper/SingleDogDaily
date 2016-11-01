b07f186eaff42efcae0e8e91f5a9d8dc63a1e99c
nick.zheng <nick.zheng@chinaredstar.com>
2016-11-01 15:18:23 +0800

    add Network Mananger Class

--- SingleDogDaily/ViewController.swift
+++ SingleDogDaily/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // default value type assign
         let label = "label width is "
+        // visible value type assign
         let width:Double = 70.0
         let widthLabel = label + String(width)
         print(widthLabel)
@@ -36,15 +38,14 @@ class ViewController: UIViewController {
             print(String(n))
         }
         
+        // traversal
         for item in 1...5 {
             print(item)
         }
         
         print(meet(name: "Bob", launch: "dumpling"))
         
-        
-        // 函数作为返回值
-        
+        // use function as return value
         func makeIncrementer() -> ((Int) -> Int) {
             func addOne (number : Int) -> Int {
                 return number + 1
@@ -54,6 +55,33 @@ class ViewController: UIViewController {
         let incrementer = makeIncrementer()
         print(incrementer(7))
         
+        // call another swift file
+        let networkMananger = NetworkManager()
+        networkMananger.login(userName: "Tommy", password: "xxxxxx")
+        
+        // enum could has its own public method
+        enum Rank: Int {
+            case Ace = 1
+            case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+            case Jack, Queen, King
+            func simpleDescription() -> String {
+                switch self {
+                case .Ace:
+                    return "Rank." + "Ace"
+                case .Jack:
+                    return "Rank." + "Jack"
+                case .Queen:
+                    return "Rank." + "Queen"
+                case .King:
+                    return "Rank." + "King"
+                default:
+                    return "Rank." + String(self.rawValue)
+                }
+            }
+        }
+        
+        let ace = Rank.Ace
+        print(ace.simpleDescription())
     }
     
     override func didReceiveMemoryWarning() {
